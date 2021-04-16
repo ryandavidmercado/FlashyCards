b5b18f0db97c9a92f409a74e5e1b6afe1160e618
ryandavidmercado <ryandavidmercado@gmail.com>
2021-04-16 12:27:22 -0400

    Dramatically simplify AutoCentered, and adjust a certain margin to compensate.

--- src/components/common/AutoCentered.js
+++ src/components/common/AutoCentered.js
@@ -1,42 +1,21 @@
-import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "react-responsive";
 import styles from "./AutoCentered.module.css";
+import classNames from "../../utils/class-names";
 
 //vertically centers content IF content is shorter than viewport
 //can optionally only center content on desktop
 //honestly the most useful code I've written in this project
 function AutoCentered({ children, requireDesktop = false }) {
-  const self = useRef(null);
   const isDesktop = useMediaQuery({ minWidth: 700 });
   const notRequired = requireDesktop && !isDesktop;
-  const [centerIt, setCenterIt] = useState(false);
-  const centerClasses = centerIt
-    ? [styles.default, styles.center]
-    : [styles.default];
-
-  useEffect(() => {
-    function handleResize() {
-      const header = document.querySelector("header");
-      const center = document.querySelector("#center-reference");
-      let toCenter = false;
-
-      if (header && center) {
-        toCenter =
-          center.scrollHeight < window.innerHeight - header.scrollHeight;
-      }
-      if (toCenter && !notRequired) setCenterIt(true);
-      else setCenterIt(false);
-    }
-
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => window.removeEventListener("resize", handleResize);
-  }, [notRequired]);
 
   return (
-    <div ref={self} className={centerClasses.join(" ")} id="center-reference">
+    <div
+      className={classNames({
+        [styles.default]: true,
+        [styles.center]: !notRequired,
+      })}
+    >
       {children}
     </div>
   );
--- src/components/common/NewButton.js
+++ src/components/common/NewButton.js
@@ -1,6 +1,7 @@
 import styles from "./NewButton.module.css";
 import add from "../../img/add.svg";
 import { Link, useParams } from "react-router-dom";
+import classNames from "../../utils/class-names";
 
 function NewButton({ type }) {
   const params = useParams();
@@ -20,7 +21,12 @@ function NewButton({ type }) {
   }
 
   return (
-    <div className={styles.buttonContainer}>
+    <div
+      className={classNames({
+        [styles.buttonContainer]: true,
+        [styles.cardButton]: type === "card",
+      })}
+    >
       <Link to={link}>
         <div className={styles.button}>
           <img src={add} width="35px" height="35px" alt=""></img>
