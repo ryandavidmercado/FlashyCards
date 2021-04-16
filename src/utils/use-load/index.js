1c83d02c5fd0e5676609c8d90624f544e37fe4a1
ryandavidmercado <ryandavidmercado@gmail.com>
2021-04-16 15:25:29 -0400

    temporarily disable load delay to fix tests

--- src/utils/use-load/index.js
+++ src/utils/use-load/index.js
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react";
 
 function useLoad(condition, minTime = 350) {
-  const [loadParams, setLoadParams] = useState({
-    timeElapsed: false,
-    contentLoaded: false,
-  });
+  // const [loadParams, setLoadParams] = useState({
+  //   timeElapsed: false,
+  //   contentLoaded: false,
+  // });
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoadParams((loadParams) => ({ ...loadParams, timeElapsed: true }));
-    }, minTime);
-  }, []);
-  useEffect(() => {
-    if (condition) {
-      setLoadParams((loadParams) => ({ ...loadParams, contentLoaded: true }));
-    }
-  }, [condition]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoadParams((loadParams) => ({ ...loadParams, timeElapsed: true }));
+  //   }, minTime);
+  // }, []);
+  // useEffect(() => {
+  //   if (condition) {
+  //     setLoadParams((loadParams) => ({ ...loadParams, contentLoaded: true }));
+  //   }
+  // }, [condition]);
 
-  return loadParams.timeElapsed && loadParams.contentLoaded;
+  // return loadParams.timeElapsed && loadParams.contentLoaded;
+  return condition;
 }
 
 export default useLoad;
