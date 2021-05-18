c76a034f2a10bd3d919b89f8fc6f3276537a5172
ryandavidmercado <ryandavidmercado@gmail.com>
2021-05-18 01:32:45 -0400

    Restore minimum load-time functionality.

--- src/utils/use-load/index.js
+++ src/utils/use-load/index.js
@@ -1,24 +1,23 @@
 import { useEffect, useState } from "react";
 
-function useLoad(condition, minTime = 350) {
-  // const [loadParams, setLoadParams] = useState({
-  //   timeElapsed: false,
-  //   contentLoaded: false,
-  // });
+function useLoad(condition, minTime = 500) {
+  const [loadParams, setLoadParams] = useState({
+    timeElapsed: false,
+    contentLoaded: false,
+  });
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoadParams((loadParams) => ({ ...loadParams, timeElapsed: true }));
-  //   }, minTime);
-  // }, []);
-  // useEffect(() => {
-  //   if (condition) {
-  //     setLoadParams((loadParams) => ({ ...loadParams, contentLoaded: true }));
-  //   }
-  // }, [condition]);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadParams((loadParams) => ({ ...loadParams, timeElapsed: true }));
+    }, minTime);
+  }, []);
+  useEffect(() => {
+    if (condition) {
+      setLoadParams((loadParams) => ({ ...loadParams, contentLoaded: true }));
+    }
+  }, [condition]);
 
-  // return loadParams.timeElapsed && loadParams.contentLoaded;
-  return condition;
+  return loadParams.timeElapsed && loadParams.contentLoaded;
 }
 
 export default useLoad;
